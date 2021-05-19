f373c7bd94c6a20b7fe92d759594bbfd48c9eb94
ShivangiBhatnagar-246 <76870700+ShivangiBhatnagar-246@users.noreply.github.com>
2021-05-19 15:07:07 +0530

    change

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".typing", {
+    strings: [ "Developer !!!", "Designer !!!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Developer !!!", "Designer !!!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
